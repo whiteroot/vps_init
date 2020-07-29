6f10c111111d589a7d224ff3b5f5c532d876fdfd
tux <tux@nunux>
2020-07-30 00:59:06 +0200

    change www directory owner

--- wp_install.sh
+++ wp_install.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+# set up apache
 read -p 'Enter your web site domain name: ' dn
 cat mywebsite.conf |sed -e "s/mydomain.com/$dn/g" > /etc/apache2/sites-available/$dn.conf
 a2ensite $dn
 systemctl restart apache2
 
+# install wordpress
 wget https://wordpress.org/latest.tar.gz
 tar -xvzf latest.tar.gz 
 mv  wordpress /var/www/$dn
+chown -R www-data:www-data /var/www/$dn
 
